a719a6313c6e51c69c2c3b8a8d1debaae4ee08ad
ZerrY <suhan0304@gmail.com>
2024-08-13 14:51:27 +0900

    Update Score & Time Logic

--- RtanRain/Assets/Scripts/GameManager.cs
+++ RtanRain/Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public GameObject rain;
+    public GameObject endPanel;
+
+    public Text totalScoreText;
+    public Text timeText;
 
     int totalScore = 0;
+    float totalTime = 30;
 
     void Awake() {
         if(Instance == null) {
@@ -23,11 +29,25 @@ public class GameManager : MonoBehaviour
         InvokeRepeating("MakeRain", 0, 0.5f);
     }
 
+    void Update()
+    {
+        if(totalTime > 0f) {
+            totalTime -= Time.deltaTime;
+            timeText.text = totalTime.ToString("N2");
+        }
+        else {
+            totalTime = 0f;
+            endPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
     void MakeRain() {
         Instantiate(rain, gameObject.transform);
     }
 
     public void AddScore(int score) {
         totalScore += score;
+        totalScoreText.text = totalScore.ToString();
     }
 }
