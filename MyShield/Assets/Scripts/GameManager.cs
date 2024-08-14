08442a60abd9c1456ba3526044112885035e1295
ZerrY <suhan0304@gmail.com>
2024-08-14 14:17:42 +0900

    Update Timer

--- MyShield/Assets/Scripts/GameManager.cs
+++ MyShield/Assets/Scripts/GameManager.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject square;
+    public Text timeText;
+
+    float time = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("MakeSquare", 0.0f, 1.0f);
+    }
+
+    void Update() {
+        time += Time.deltaTime;
+        timeText.text = time.ToString("N2");
     }
 
     void MakeSquare() {
