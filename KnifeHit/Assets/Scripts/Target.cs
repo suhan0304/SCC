6d71242cdc560c89dbb06ec1aa0d980963d60bbf
ZerrY <suhan0304@gmail.com>
2024-08-21 16:00:58 +0900

    Update Target.cs

--- KnifeHit/Assets/Scripts/Target.cs
+++ KnifeHit/Assets/Scripts/Target.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using JetBrains.Annotations;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public float minRotation = 0f;
-    public float maxRotation = 720f;
-    public float duration = 1f;
+    [TabGroup("Tap","Rotate",SdfIconType.CodeSlash, TextColor="Green")]
+    
+    [TabGroup("Tap","Rotate")] public float minRotation = 90f;
 
+    [TabGroup("Tap","Rotate")] public float maxRotation = 720f;
+
+    
+    [TabGroup("Tap","Rotate")] public float minDuration = 1f;
+    [TabGroup("Tap","Rotate")]  public float maxDuration = 3f;
+
+    
+    [TabGroup("Tap","Rotate")] public float maxRotationSpeed = 360f;
+
+    [TabGroup("Tap","ReadOnly"), ReadOnly] public float targetRotation = 0;
+
+    [TabGroup("Tap","Rotate")] 
     [Button("Rotate Target")]
     public void RotateTarget() {
         float randomZRotation = Random.Range(minRotation, maxRotation);
 
-        transform.DORotate(new Vector3(0f, 0f, randomZRotation), duration, RotateMode.FastBeyond360)
-            .SetEase(Ease.InOutSine)
-            .OnComplete(() => Debug.Log("Rotation Complete!"));
+        float rotationDirection = Random.Range(0, 2) * 2 - 1;
+
+        targetRotation = transform.eulerAngles.z + rotationDirection * (randomZRotation - transform.eulerAngles.z);
+        
+
+        float rotationDistance = Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetRotation));
+        float calculatedDurtaion = Mathf.Clamp(rotationDistance / maxRotationSpeed, minDuration, maxDuration);
+
+        transform.DORotate(new Vector3(0f, 0f, targetRotation), calculatedDurtaion, RotateMode.FastBeyond360)
+            .SetEase(Ease.InOutSine);
+            //.OnComplete(() => Debug.Log("Rotation Complete!"));
     }
 }
