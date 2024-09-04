8505986dc8d628a8082558abc3fa5daac82bcfad
ZerrY <suhan0304@gmail.com>
2024-09-04 16:23:50 +0900

    Update Singleton.cs

--- KnifeHit/Assets/Scripts/MainScene/Target/Singleton.cs
+++ KnifeHit/Assets/Scripts/MainScene/Target/Singleton.cs
@@ -5,9 +5,10 @@ class Singleton {
     // static 내부 클래스를 이용
     // Holder로 만들어, 클래스가 메모리에 로드되지 않고 getInstance 메서드가 호출되어야 로드됨
     private static class SingleInstanceHolder {
-        private static final Singleton INSTANCE = new Singleton();
+        private static readonly Singleton INSTANCE = new Singleton();
     }
 
     public static Singleton getInstance() {
         return SingleInstanceHolder.INSTANCE;
     }
+}
