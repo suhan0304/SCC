1393ec98d0cf1e58f0fa2605eae7edbd1001196d
ZerrY <suhan0304@gmail.com>
2024-09-04 15:44:50 +0900

    Update Singleton.cs

--- KnifeHit/Assets/Scripts/MainScene/Target/Singleton.cs
+++ KnifeHit/Assets/Scripts/MainScene/Target/Singleton.cs
@@ -1,15 +1,12 @@
 class Singleton {
-    // 싱글톤 객체를 담을 인스턴스 변수
-    private static Singleton instance;
+    private static Singleton INSTANCE;
 
-    // 생성자는 private로 선언 
     private Singleton() {}
-	
-    // 외부에서 정적 메서드를 호출하면 초기화 진행
-    public static Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton(); // 오직 1개의 객체만 생성
+
+    public static synchronized Singleton getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Singleton();
         }
-        return instance;
+        return INSTANCE;
     }
 }
