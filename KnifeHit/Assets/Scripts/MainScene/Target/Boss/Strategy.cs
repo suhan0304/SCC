81552463bc0ad7333c7a4515588e8912666e7f53
ZerrY <suhan0304@gmail.com>
2024-09-05 11:55:23 +0900

    Update Strategy.cs

--- KnifeHit/Assets/Scripts/MainScene/Target/Boss/Strategy.cs
+++ KnifeHit/Assets/Scripts/MainScene/Target/Boss/Strategy.cs
@@ -1,15 +1,17 @@
-public interface MovableStrategy {
+class Strategy() {
+    public interface MovableStrategy {
     public void move();
 }
 
-public class RailLoadStrategy implements MovableStrategy{
-    public void move(){
-        System.out.println("선로를 통해 이동");
+    public class RailLoadStrategy implements MovableStrategy{
+        public void move(){
+            System.out.println("선로를 통해 이동");
+        }
     }
-}
 
-public class LoadStrategy implements MovableStrategy{
-    public void move() {
-        System.out.println("도로를 통해 이동");
+    public class LoadStrategy implements MovableStrategy{
+        public void move() {
+            System.out.println("도로를 통해 이동");
+        }
     }
 }
