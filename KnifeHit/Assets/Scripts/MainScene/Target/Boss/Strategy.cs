2333c0c013ba5d0a2c2f9ba64299323f6aa7b7c2
ZerrY <suhan0304@gmail.com>
2024-09-05 11:58:16 +0900

    Update Strategy.cs

--- KnifeHit/Assets/Scripts/MainScene/Target/Boss/Strategy.cs
+++ KnifeHit/Assets/Scripts/MainScene/Target/Boss/Strategy.cs
@@ -15,3 +15,15 @@ class Strategy() {
         }
     }
 }
+
+public class Moving {
+    private MovableStrategy movableStrategy;
+
+    public void move(){
+        movableStrategy.move();
+    }
+
+    public void setMovableStrategy(MovableStrategy movableStrategy){
+        this.movableStrategy = movableStrategy;
+    }
+}
