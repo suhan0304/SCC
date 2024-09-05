ef12e95d9577614a7f457033b691513b1931f0fd
ZerrY <suhan0304@gmail.com>
2024-09-05 11:55:05 +0900

    Update Strategy.cs

--- KnifeHit/Assets/Scripts/MainScene/Target/Boss/Strategy.cs
+++ KnifeHit/Assets/Scripts/MainScene/Target/Boss/Strategy.cs
@@ -1,3 +1,9 @@
 public interface MovableStrategy {
     public void move();
 }
+
+public class RailLoadStrategy implements MovableStrategy{
+    public void move(){
+        System.out.println("선로를 통해 이동");
+    }
+}
